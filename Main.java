49e4da238ebd3039e8ba3138abab392c93506ec1
PavelStici <stici.pavel2002@gmail.com>
2024-03-26 11:35:01 +0200

    Laboratorul 3 finalizat si prezentat

--- Main.java
+++ Main.java
@@ -10,7 +10,8 @@ class Linie
         System.out.println("Constructor Linie()");
     }
 
-    String modifyString(String input) {
+    String modifyString(String input) 
+    {
         String escapedInput = input.replaceAll("([\\\\\\[\\](){}+*?.^$|])", "\\\\$1");
         
         String manipulatedString = escapedInput.replaceAll("\\b(\\w+)\\b", "\\\\b$1\\\\b");
@@ -18,19 +19,24 @@ class Linie
         return manipulatedString;
     }
 
-    String modifyString2(String input) {
+    protected String modifyString2(String input) 
+    {
         StringBuilder modifiedString = new StringBuilder();
         
-        for (int i = 0; i < input.length(); i++) {
+        for (int i = 0; i < input.length(); i++) 
+        {
             char currentChar = input.charAt(i);
             
-            if (isSpecialCharacter(currentChar)) {
+            if (isSpecialCharacter(currentChar)) 
+            {
                 modifiedString.append("\\").append(currentChar);
             }
 
-            if (Character.isLetterOrDigit(currentChar)) {
+            if (Character.isLetterOrDigit(currentChar)) 
+            {
                 int start = i;
-                while (i < input.length() && Character.isLetterOrDigit(input.charAt(i))) {
+                while (i < input.length() && Character.isLetterOrDigit(input.charAt(i))) 
+                {
                     i++;
                 }
                 modifiedString.append("\\b").append(input.substring(start, i)).append("\\b");
@@ -42,7 +48,8 @@ class Linie
         return modifiedString.toString();
     }
 
-    boolean isSpecialCharacter(char c) {
+    protected boolean isSpecialCharacter(char c) 
+    {
         return c == '\\' || c == '[' || c == ']' || c == '(' || c == ')' ||
                c == '{' || c == '}' || c == '+' || c == '*' || c == '?' ||
                c == '.' || c == '^' || c == '$' || c == '|' || c == '-';
@@ -82,9 +89,11 @@ class Linie
         System.out.println("regex = " + regex);
         Pattern p = Pattern.compile(regex, Pattern.CASE_INSENSITIVE);
         Matcher m = p.matcher(text);
-        if (m.find()) {
+        if (m.find()) 
+        {
             System.out.println("Sirul a fost gasit in linie : " + pattern);
-        } else {
+        } else 
+        {
             System.out.println("Sirul nu a fost gasit in linie");
         }
     }
@@ -92,52 +101,63 @@ class Linie
 
 class Pagina extends Linie { 
     
-    Pagina() {
+    Pagina() 
+    {
         System.out.println("Constructor Pagina()");
     }
 
-    Pagina(String text) {
+    Pagina(String text) 
+    {
         super(text);
         System.out.println("Constructor Pagina(String text), text = " + text);
     }
 
-    @Override
-    void setText(String text) {
+
+    void setText(String text) 
+    {
         this.text = text;
         System.out.println("Pagina.setText(String text), text = " + text);
     }
 
     @Override
-    void afisare() {
+    void afisare() 
+    {
         System.out.println("Text din pagină: \n" + text);
     }
 
     @Override
-    void cautare(String pattern) {
-
+    void cautare(String pattern) 
+    {
         String regex = "(?<!\\S)" + modifyString2(pattern) + "(?!\\S)";
+
         System.out.println("regex = " + regex);
+
         Pattern p = Pattern.compile(regex, Pattern.CASE_INSENSITIVE);
         Matcher m = p.matcher(text);
-        if (m.find()) {
+
+        if (m.find()) 
+        {
             System.out.println("Sirul a fost gasit in pagina : " + pattern);
-        } else {
+        } else 
+        {
             System.out.println("Sirul nu a fost gasit in pagina");
         }
     }
 }
 
-public class Main {
-    public static void main(String[] args) {
-        Pagina pagina = new Pagina();
+public class Main 
+{
+    public static void main(String[] args) 
+    {
+        //Pagina pagina = new Pagina();
 
-        pagina.setText("Lorem ipsum dolor sit amet. consectetur adipiscing elit.");
-        pagina.afisare("Textul este: ");
-        pagina.cautare("amet.");
+        // pagina.setText("Lorem ipsum dolor sit amet. consectetur adipiscing elit.");
+        // pagina.afisare("Textul este: ");
+        // pagina.cautare("amet.");
 
-        Linie linie = new Linie("Lorem ipsum.-s dolor sit amet.");
+        Linie linie = new Linie("Lorem ana-maria dolor sit amet.");
         linie.afisare();
-        linie.cautare("ipsum.-s");
+        linie.cautare("ana-ma");
 
     }
 }
